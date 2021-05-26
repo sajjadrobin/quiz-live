7ffa12cd429fe23a71b03398e737d3a4c6eeafd1
Robin Sajjadul <sajjadul.robin@bontouch.com>
2021-05-26 09:34:26 +0000

    BUSER-165-Firebase BBU Live URL

--- src/AgoraSettings.js
+++ src/AgoraSettings.js
@@ -1,7 +1,7 @@
 const clientOptions = {
   appId: "530e02bb27b840258e2da5fc229aa700",
   channel: "myChannel",
-  token: "006530e02bb27b840258e2da5fc229aa700IADYEPQrqfTklBzFbBflWsGU0uNbNMID4wReuS1V1jxAkEOQEggAAAAAEABpDq0kdvCtYAEAAQB18K1g",
+  token: "006530e02bb27b840258e2da5fc229aa700IAClB2YTYQo7+2P1DXFV7AmNLXV4Ftd9c00QYZTLTGrQ8EOQEggAAAAAEAB+PjEWhlCvYAEAAQCGUK9g",
   uid: null,
   role: "host",
 };
