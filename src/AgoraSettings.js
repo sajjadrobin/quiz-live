91235f4f085b733814ed425a1303c7005adf7a55
Sajjad Robin <technovicebd@gmail.com>
2024-08-12 11:56:59 +0200

    Agora App changed

--- src/AgoraSettings.js
+++ src/AgoraSettings.js
@@ -2,7 +2,7 @@ const clientOptions = {
   appId: "ef394f7dc5134ee180f1ead957784a84",
   role: "host",
   agoraToken:
-    "007eJxTYDgbsLr71qfHhhWFdk85WLbIWC13XaK343Pf95qngdVXMpoVGFLTjC1N0sxTkk0NjU1SUw0tDNIMUxNTLE3NzS1MEi1MWuo5kpkvciYHyTWxMjJAIIjPwlBYmlnFwAAAyJQg+w==",
+    "007eJxTYPjvEck+e9nCDbffuTypW/Hm6a1GpyubFM71s3p4WESxNt1RYEhNM7Y0STNPSTY1NDZJTTW0MEgzTE1MsTQ1N7cwSbQw4X26M60hkJHhxg5WZkYGCATxWRgKSzOrGBgA+/0hfw==",
   channel: "quiz",
 };
 
