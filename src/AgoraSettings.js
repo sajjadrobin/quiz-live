aca0096ba1233050bb6d77178bc666e82089a5ca
Md Sajjadul Farooque Robin <sajjadrobin@users.noreply.github.com>
2024-08-23 09:38:09 +0200

    Update AgoraSettings.js
    Token updated

--- src/AgoraSettings.js
+++ src/AgoraSettings.js
@@ -2,7 +2,7 @@ const clientOptions = {
   appId: "ef394f7dc5134ee180f1ead957784a84",
   role: "host",
   agoraToken:
-    "007eJxTYPjvEck+e9nCDbffuTypW/Hm6a1GpyubFM71s3p4WESxNt1RYEhNM7Y0STNPSTY1NDZJTTW0MEgzTE1MsTQ1N7cwSbQw4X26M60hkJHhxg5WZkYGCATxWRgKSzOrGBgA+/0hfw==",
+    "007eJxTYDCftPSkFYeiduEqpukT/P9ct9l+f2Gpb7mTzkc31k0XG1MUGFLTjC1N0sxTkk0NjU1SUw0tDNIMUxNTLE3NzS1MEi1MxG1OpDUEMjIUbFnJxMgAgSA+C0NhaWYVAwMACg0exA==",
   channel: "quiz",
 };
 
